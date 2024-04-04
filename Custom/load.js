e582e1b3b6dc3c2a53951aa87707235e4d7236f2
CDo-GammaSci <cdo@gamma-sci.com>
2024-04-04 16:04:43 -0700

    adding black pattern

--- Custom/load.js
+++ Custom/load.js
@@ -1,25 +1,15 @@
 /* Loads in prebuilt scenes and adds them to scenes json */
-
+scenes['default']['FS_Texture'] = FS_Texture['scenes']['FS_Texture']
 scenes['default']['red'] = red['scenes']['default']
-
 scenes['default']['green'] = green['scenes']['default']
-
 scenes['default']['blue'] = blue['scenes']['default']
-
-scenes['default']['white'] = white['scenes']['default']
-
+scenes['default']['black'] = blue['scenes']['default']
 scenes['default']['grille'] = grille['scenes']['default']
-
 scenes['default']['line'] = line['scenes']['line']
-
 scenes['default']['checkerboard_w'] = checkerboard_w['scenes']['checkerboard_w']
-
 scenes['default']['checkerboard_b'] = checkerboard_b['scenes']['checkerboard_b']
-
 scenes['default']['dot array'] = dot_array['scenes']['dot array']
-
 scenes['default']['circular dot array'] = circular_dot_array['scenes']['circularDotArray']
-
 scenes['default']['crosshair'] = crosshair['scenes']['crosshair']
 
 /*import bullseye from './patterns/bullseye.JSON' assert { type: "json" };
@@ -31,14 +21,30 @@ scenes['default']['ring_w10'] = ring_package['scenes']['ring_w10']
 scenes['default']['ring_w20'] = ring_package['scenes']['ring_w20']*/
 
 scenes['default']['ring_w1'] = ring_w1['scenes']['ring_w1']
-
 scenes['default']['ring_w2'] = ring_w2['scenes']['ring_w2']
-
 scenes['default']['ring_w5'] = ring_w5['scenes']['ring_w5']
-
 scenes['default']['Flying Spot'] = flying_spot['scenes']['Flying Spot']
 
-scenes['default']['FS_Texture'] = FS_Texture['scenes']['FS_Texture']
+
+
+// Meta quest specific
+scenes['meta_quest']['FS_Texture'] = FS_Texture['scenes']['FS_Texture']
+scenes['meta_quest']['red'] = red['scenes']['default']
+scenes['meta_quest']['green'] = green['scenes']['default']
+scenes['meta_quest']['blue'] = blue['scenes']['default']
+scenes['meta_quest']['grille'] = grille['scenes']['default']
+scenes['default']['black'] = blue['scenes']['default']
+scenes['meta_quest']['line'] = line['scenes']['line']
+scenes['meta_quest']['checkerboard_w'] = checkerboard_w['scenes']['checkerboard_w']
+scenes['meta_quest']['checkerboard_b'] = checkerboard_b['scenes']['checkerboard_b']
+scenes['meta_quest']['dot array'] = dot_array['scenes']['dot array']
+scenes['meta_quest']['circular dot array'] = circular_dot_array['scenes']['circularDotArray']
+scenes['meta_quest']['crosshair'] = crosshair['scenes']['crosshair']
+scenes['meta_quest']['ring_w1'] = ring_w1['scenes']['ring_w1']
+scenes['meta_quest']['ring_w2'] = ring_w2['scenes']['ring_w2']
+scenes['meta_quest']['ring_w5'] = ring_w5['scenes']['ring_w5']
+scenes['meta_quest']['Flying Spot'] = flying_spot['scenes']['Flying Spot']
+
 
 console.log('Scenes')
 console.log(scenes)
