098bf3270008d6aa1ee23c0670a4af5a39d3557b
CDo-GammaSci <122911625+CDo-GammaSci@users.noreply.github.com>
2024-04-05 00:52:39 -0700

    update full screen patterns

--- Custom/patterns/green.js
+++ Custom/patterns/green.js
@@ -2,7 +2,62 @@ var green = {
 	"scenes": {
 		"default": {
 			"sky": {
-				"skyColor": "#00ff00"
+				"skyColor": "#000000"
+			},
+			"plane0": {
+				"advanced": {
+					"val": true
+				},
+				"angle": {
+					"x": 0,
+					"z": -125
+				},
+				"widthReal": 119.271,
+				"fill": {
+					"val": 119.27083333333334,
+					"isFull": true
+				},
+				"geometry": {
+					"primitive": "plane",
+					"width": 119.271,
+					"height": 125
+				},
+				"position": {
+					"x": 0,
+					"y": 0,
+					"z": -125
+				},
+				"material": {
+					"shader": "flat",
+					"color": "#00gg00",
+					"src": "#FS_W"
+				},
+				"rotation": {
+					"x": 0,
+					"y": 0,
+					"z": 0
+				},
+				"movement": {
+					"startPoints": [],
+					"endPoints": [],
+					"initialVelocities": [],
+					"accelerations": [],
+					"types": [],
+					"origin": {
+						"x": 0,
+						"y": 0,
+						"z": -125
+					},
+					"rotationOrigin": {
+						"x": 0,
+						"y": 0,
+						"z": 0
+					},
+					"status": -1,
+					"index": 0,
+					"currentVelocity": 0,
+					"timeElapsed": -1
+				}
 			}
 		}
 	},
@@ -14,32 +69,8 @@ var green = {
 				"text": "none"
 			},
 			{
-				"val": "QC",
-				"text": "TG18-QC.2k_12b"
-			},
-			{
-				"val": "CH",
-				"text": "TG18-CH.2k"
-			},
-			{
-				"val": "MM1",
-				"text": "TG18-MM1.2k"
-			},
-			{
-				"val": "MM2",
-				"text": "TG18-MM2.2k"
-			},
-			{
-				"val": "sQC",
-				"text": "TG270sQC"
-			},
-			{
-				"val": "PQC",
-				"text": "TG18-PQC.2k_12b"
-			},
-			{
-				"val": "BR",
-				"text": "TG18-BR.2k_12b"
+				"val": "FS_W",
+				"text": "FullScreen_White"
 			}
 		]
 	},
