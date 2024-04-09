47a105cb7dbeed5899f37199706064feccf8870d
CDo-GammaSci <cdo@gamma-sci.com>
2024-04-09 12:10:20 -0700

    add remaining vbar and hbar patterns

--- Custom/load.js
+++ Custom/load.js
@@ -41,10 +41,212 @@ scenes['default']['VBar 6'] = vbar_6['scenes']['default'];
 import vbar_7 from './patterns/vbar7.JSON' assert { type: "json" };
 scenes['default']['VBar 7'] = vbar_7['scenes']['default'];
 
+import vbar_8 from './patterns/vbar8.JSON' assert { type: "json" };
+scenes['default']['VBar 8'] = vbar_8['scenes']['default'];
+
+import vbar_9 from './patterns/vbar9.JSON' assert { type: "json" };
+scenes['default']['VBar 9'] = vbar_9['scenes']['default'];
+
+import vbar_10 from './patterns/vbar10.JSON' assert { type: "json" };
+scenes['default']['VBar 10'] = vbar_10['scenes']['default'];
+
+import vbar_11 from './patterns/vbar11.JSON' assert { type: "json" };
+scenes['default']['VBar 11'] = vbar_11['scenes']['default'];
+
+import vbar_12 from './patterns/vbar12.JSON' assert { type: "json" };
+scenes['default']['VBar 12'] = vbar_12['scenes']['default'];
+
+import vbar_13 from './patterns/vbar13.JSON' assert { type: "json" };
+scenes['default']['VBar 13'] = vbar_13['scenes']['default'];
+
+import vbar_14 from './patterns/vbar14.JSON' assert { type: "json" };
+scenes['default']['VBar 14'] = vbar_14['scenes']['default'];
+
+import vbar_15 from './patterns/vbar15.JSON' assert { type: "json" };
+scenes['default']['VBar 15'] = vbar_15['scenes']['default'];
+
+import vbar_16 from './patterns/vbar16.JSON' assert { type: "json" };
+scenes['default']['VBar 16'] = vbar_16['scenes']['default'];
+
+import vbar_17 from './patterns/vbar17.JSON' assert { type: "json" };
+scenes['default']['VBar 17'] = vbar_17['scenes']['default'];
+
+import vbar_18 from './patterns/vbar18.JSON' assert { type: "json" };
+scenes['default']['VBar 18'] = vbar_18['scenes']['default'];
+
+import vbar_19 from './patterns/vbar19.JSON' assert { type: "json" };
+scenes['default']['VBar 19'] = vbar_19['scenes']['default'];
+
 import vbar_20 from './patterns/vbar20.JSON' assert { type: "json" };
 scenes['default']['VBar 20'] = vbar_20['scenes']['default'];
 
+import vbar_21 from './patterns/vbar21.JSON' assert { type: "json" };
+scenes['default']['VBar 21'] = vbar_21['scenes']['default'];
 
+import vbar_22 from './patterns/vbar22.JSON' assert { type: "json" };
+scenes['default']['VBar 22'] = vbar_22['scenes']['default'];
+
+import vbar_23 from './patterns/vbar23.JSON' assert { type: "json" };
+scenes['default']['VBar 23'] = vbar_23['scenes']['default'];
+
+import vbar_24 from './patterns/vbar24.JSON' assert { type: "json" };
+scenes['default']['VBar 24'] = vbar_24['scenes']['default'];
+
+import vbar_25 from './patterns/vbar25.JSON' assert { type: "json" };
+scenes['default']['VBar 25'] = vbar_25['scenes']['default'];
+
+import vbar_26 from './patterns/vbar26.JSON' assert { type: "json" };
+scenes['default']['VBar 26'] = vbar_26['scenes']['default'];
+
+import vbar_27 from './patterns/vbar27.JSON' assert { type: "json" };
+scenes['default']['VBar 27'] = vbar_27['scenes']['default'];
+
+import vbar_28 from './patterns/vbar28.JSON' assert { type: "json" };
+scenes['default']['VBar 28'] = vbar_28['scenes']['default'];
+
+import vbar_29 from './patterns/vbar29.JSON' assert { type: "json" };
+scenes['default']['VBar 29'] = vbar_29['scenes']['default'];
+
+import vbar_30 from './patterns/vbar30.JSON' assert { type: "json" };
+scenes['default']['VBar 30'] = vbar_30['scenes']['default'];
+
+import vbar_31 from './patterns/vbar31.JSON' assert { type: "json" };
+scenes['default']['VBar 31'] = vbar_31['scenes']['default'];
+
+import vbar_32 from './patterns/vbar32.JSON' assert { type: "json" };
+scenes['default']['VBar 32'] = vbar_32['scenes']['default'];
+
+import vbar_33 from './patterns/vbar33.JSON' assert { type: "json" };
+scenes['default']['VBar 33'] = vbar_33['scenes']['default'];
+
+import vbar_34 from './patterns/vbar34.JSON' assert { type: "json" };
+scenes['default']['VBar 34'] = vbar_34['scenes']['default'];
+
+import vbar_35 from './patterns/vbar35.JSON' assert { type: "json" };
+scenes['default']['VBar 35'] = vbar_35['scenes']['default'];
+
+import vbar_36 from './patterns/vbar36.JSON' assert { type: "json" };
+scenes['default']['VBar 36'] = vbar_36['scenes']['default'];
+
+import vbar_37 from './patterns/vbar37.JSON' assert { type: "json" };
+scenes['default']['VBar 37'] = vbar_37['scenes']['default'];
+
+import vbar_38 from './patterns/vbar38.JSON' assert { type: "json" };
+scenes['default']['VBar 38'] = vbar_38['scenes']['default'];
+
+import vbar_39 from './patterns/vbar39.JSON' assert { type: "json" };
+scenes['default']['VBar 39'] = vbar_39['scenes']['default'];
+
+import vbar_40 from './patterns/vbar40.JSON' assert { type: "json" };
+scenes['default']['VBar 40'] = vbar_40['scenes']['default'];
+
+import hbar_5 from './patterns/hbar5.JSON' assert { type: "json" };
+scenes['default']['HBar 5'] = hbar_5['scenes']['default'];
+
+import hbar_6 from './patterns/hbar6.JSON' assert { type: "json" };
+scenes['default']['HBar 6'] = hbar_6['scenes']['default'];
+
+import hbar_7 from './patterns/hbar7.JSON' assert { type: "json" };
+scenes['default']['HBar 7'] = hbar_7['scenes']['default'];
+
+import hbar_8 from './patterns/hbar8.JSON' assert { type: "json" };
+scenes['default']['HBar 8'] = hbar_8['scenes']['default'];
+
+import hbar_9 from './patterns/hbar9.JSON' assert { type: "json" };
+scenes['default']['HBar 9'] = hbar_9['scenes']['default'];
+
+import hbar_10 from './patterns/hbar10.JSON' assert { type: "json" };
+scenes['default']['HBar 10'] = hbar_10['scenes']['default'];
+
+import hbar_11 from './patterns/hbar11.JSON' assert { type: "json" };
+scenes['default']['HBar 11'] = hbar_11['scenes']['default'];
+
+import hbar_12 from './patterns/hbar12.JSON' assert { type: "json" };
+scenes['default']['HBar 12'] = hbar_12['scenes']['default'];
+
+import hbar_13 from './patterns/hbar13.JSON' assert { type: "json" };
+scenes['default']['HBar 13'] = hbar_13['scenes']['default'];
+
+import hbar_14 from './patterns/hbar14.JSON' assert { type: "json" };
+scenes['default']['HBar 14'] = hbar_14['scenes']['default'];
+
+import hbar_15 from './patterns/hbar15.JSON' assert { type: "json" };
+scenes['default']['HBar 15'] = hbar_15['scenes']['default'];
+
+import hbar_16 from './patterns/hbar16.JSON' assert { type: "json" };
+scenes['default']['HBar 16'] = hbar_16['scenes']['default'];
+
+import hbar_17 from './patterns/hbar17.JSON' assert { type: "json" };
+scenes['default']['HBar 17'] = hbar_17['scenes']['default'];
+
+import hbar_18 from './patterns/hbar18.JSON' assert { type: "json" };
+scenes['default']['HBar 18'] = hbar_18['scenes']['default'];
+
+import hbar_19 from './patterns/hbar19.JSON' assert { type: "json" };
+scenes['default']['HBar 19'] = hbar_19['scenes']['default'];
+
+import hbar_20 from './patterns/hbar20.JSON' assert { type: "json" };
+scenes['default']['HBar 20'] = hbar_20['scenes']['default'];
+
+import hbar_21 from './patterns/hbar21.JSON' assert { type: "json" };
+scenes['default']['HBar 21'] = hbar_21['scenes']['default'];
+
+import hbar_22 from './patterns/hbar22.JSON' assert { type: "json" };
+scenes['default']['HBar 22'] = hbar_22['scenes']['default'];
+
+import hbar_23 from './patterns/hbar23.JSON' assert { type: "json" };
+scenes['default']['HBar 23'] = hbar_23['scenes']['default'];
+
+import hbar_24 from './patterns/hbar24.JSON' assert { type: "json" };
+scenes['default']['HBar 24'] = hbar_24['scenes']['default'];
+
+import hbar_25 from './patterns/hbar25.JSON' assert { type: "json" };
+scenes['default']['HBar 25'] = hbar_25['scenes']['default'];
+
+import hbar_26 from './patterns/hbar26.JSON' assert { type: "json" };
+scenes['default']['HBar 26'] = hbar_26['scenes']['default'];
+
+import hbar_27 from './patterns/hbar27.JSON' assert { type: "json" };
+scenes['default']['HBar 27'] = hbar_27['scenes']['default'];
+
+import hbar_28 from './patterns/hbar28.JSON' assert { type: "json" };
+scenes['default']['HBar 28'] = hbar_28['scenes']['default'];
+
+import hbar_29 from './patterns/hbar29.JSON' assert { type: "json" };
+scenes['default']['HBar 29'] = hbar_29['scenes']['default'];
+
+import hbar_30 from './patterns/hbar30.JSON' assert { type: "json" };
+scenes['default']['HBar 30'] = hbar_30['scenes']['default'];
+
+import hbar_31 from './patterns/hbar31.JSON' assert { type: "json" };
+scenes['default']['HBar 31'] = hbar_31['scenes']['default'];
+
+import hbar_32 from './patterns/hbar32.JSON' assert { type: "json" };
+scenes['default']['HBar 32'] = hbar_32['scenes']['default'];
+
+import hbar_33 from './patterns/hbar33.JSON' assert { type: "json" };
+scenes['default']['HBar 33'] = hbar_33['scenes']['default'];
+
+import hbar_34 from './patterns/hbar34.JSON' assert { type: "json" };
+scenes['default']['HBar 34'] = hbar_34['scenes']['default'];
+
+import hbar_35 from './patterns/hbar35.JSON' assert { type: "json" };
+scenes['default']['HBar 35'] = hbar_35['scenes']['default'];
+
+import hbar_36 from './patterns/hbar36.JSON' assert { type: "json" };
+scenes['default']['HBar 36'] = hbar_36['scenes']['default'];
+
+import hbar_37 from './patterns/hbar37.JSON' assert { type: "json" };
+scenes['default']['HBar 37'] = hbar_37['scenes']['default'];
+
+import hbar_38 from './patterns/hbar38.JSON' assert { type: "json" };
+scenes['default']['HBar 38'] = hbar_38['scenes']['default'];
+
+import hbar_39 from './patterns/hbar39.JSON' assert { type: "json" };
+scenes['default']['HBar 39'] = hbar_39['scenes']['default'];
+
+import hbar_40 from './patterns/hbar40.JSON' assert { type: "json" };
+scenes['default']['HBar 40'] = hbar_40['scenes']['default'];
 
 
 console.log('Scenes')
